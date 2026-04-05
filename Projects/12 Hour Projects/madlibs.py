3f0a53e73a8bbfc979c12906c0286538115fa586
TheoIsADev <theo.morin20@gmail.com>
2026-04-05 18:04:41 -0400

    Created a madlibs game

---
+++ Projects/12 Hour Projects/madlibs.py
@@ -0,0 +1,10 @@
+# Madlibs game
+
+place = input("Enter a place: ")
+person = input("Enter a name: ")
+action = input("Enter an action: ")
+
+print(f"Today, I went to {place}.")
+print(f"It was strange because {person} was there.")
+print(f"He {action} and it was impressive.")
+print(f"I will never forget that moment!")
