07b1b883ebd5899726bc753dd10f5a7d43b3caa5
TheoIsADev <theo.morin20@gmail.com>
2026-04-07 21:11:48 -0400

    Added comments to madlibs.py

--- Projects/12 Hour Projects/madlibs.py
+++ Projects/12 Hour Projects/madlibs.py
@@ -1,9 +1,11 @@
 # Madlibs game
 
+# Creating variables for user's input
 place = input("Enter a place: ")
 person = input("Enter a name: ")
 action = input("Enter an action: ")
 
+# Printing the story according to the variables
 print(f"Today, I went to {place}.")
 print(f"It was strange because {person} was there.")
 print(f"He {action} and it was impressive.")
